7397f11c0713f916cd81362e00f83eafcee7812c
pavikrnm89 <pavankarnam89@gmail.com>
2017-01-04 11:15:44 +0530

    Update app.js

--- resources/app.js
+++ resources/app.js
@@ -1,1 +1,11 @@
-
+function circle(radius)
+{
+    this.radius = radius;
+  // area method
+    this.area = function () 
+    {
+        return Math.PI * this.radius * this.radius;
+    };
+}
+var c = new circle(3);
+console.log('Area =', c.area().toFixed(2));
